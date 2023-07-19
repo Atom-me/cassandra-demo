4ec03ab8b75752bb128685dab9983a467b8f3931
Atom <sarming@126.com>
2023-07-19 20:42:25 +0800

    test

--- src/main/java/com/atom/cassandra/CassandraDemoApplication.java
+++ src/main/java/com/atom/cassandra/CassandraDemoApplication.java
@@ -1,13 +1,10 @@
 package com.atom.cassandra;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CassandraDemoApplication {
-    private final static Logger log = LoggerFactory.getLogger(CassandraDemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(CassandraDemoApplication.class, args);
